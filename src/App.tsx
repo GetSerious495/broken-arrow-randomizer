35084f1cae40f1ead067add35c74d79089f34ea7
GetSerious <getserious.495@gmail.com>
2025-12-26 23:15:20 +0700

    UI work

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { useState } from "react";
 import './App.css';
 
 const App = () => {
-  const [state, setState] = useState(0);
 
   return (
     <>
-      <h1>App</h1>
-      <button onClick={() => setState(state + 1)}></button>
-      <button onClick={() => setState(state - 1)}></button>
-      <button onClick={() => setState(state * 0)}></button>
-      <p>{state}</p>
+      <div className="container">
+
+        <div className='titlecard'>
+          <h1 className='title'>Broken Arrow Randomizer</h1>
+        </div>
+
+        <div className="option-1">
+          <h2 className='option-text'>Choose your Nation</h2>
+        </div>
+
+        <div className="nation-select">
+          
+        </div>
+      </div>
     </>
   );
 };
